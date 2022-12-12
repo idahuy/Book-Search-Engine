1e767b19bc9b0b6659baa45847182596efcc6baf
Huy <drac0123451@gmail.com>
2022-12-12 01:54:09 -0800

    removed unnecessary code from server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,9 +21,6 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-//apply apollo server with express app
-server.applyMiddleware({ app });
-
 //middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,20 +30,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
-
 //get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+};
+
+startApolloServer(typeDefs, resolvers);
